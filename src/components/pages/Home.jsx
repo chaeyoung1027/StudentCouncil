a4c9a8d885b711fbc796f5e1ffb30eb784ff7e5f
Kim-yujin <w2117@e-mirim.hs.kr>
2023-03-07 23:47:22 +0900

    HomePage edit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import image from '../../img/mainpage.png';
 
 function Home(props) {
     return (
-        <body style={{bgcolor: 'black',}}>
-            <h1>hello</h1>
-        </body>
+        <Container>
+            <Img src ={image}></Img>
+        </Container>
     )
 }
-
+const Container = styled.div`
+  top: 0;
+  width: 100vw;
+  overflow: hidden;
+  height: 100vh;
+  background-color: #000;
+`;
+const Img = styled.img`
+    margin: 0 auto;
+    width: 52vw;
+    display: flex;
+    justify-content: center;
+    margin-top: -2.5vw;`;
 export default Home;
