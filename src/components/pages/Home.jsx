83d8e7b10664b0353ae7e2435062d31c73221289
Kim-yujin <w2117@e-mirim.hs.kr>
2023-03-08 00:55:10 +0900

    Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
 import image from '../../img/mainpage.png';
+import imagecircle from '../../img/circle.png';
+import './home.css';
 
 function Home(props) {
     return (
         <Container>
             <Img src ={image}></Img>
+            <Img2 src ={imagecircle}></Img2>
+            <Text>MIRIM ALBUM</Text>
         </Container>
     )
 }
@@ -14,12 +18,30 @@ const Container = styled.div`
   width: 100vw;
   overflow: hidden;
   height: 100vh;
-  background-color: #000;
+  background-color: #161616;
 `;
 const Img = styled.img`
     margin: 0 auto;
-    width: 52vw;
+    width: 63vw;
     display: flex;
     justify-content: center;
     margin-top: -2.5vw;`;
+const Img2 = styled.img`
+    width: 43vw;
+    height: 43vw;
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    margin-top: -53vw;`;
+const Text = styled.div`
+    font-size: 4.5vw;
+    letter-spacing: 0.65rem;
+    font-weight: bold;
+    font-family: 'Noto Sans KR', sans-serif;
+    color: #F5F8FF;  
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    margin-top: -25vw;  
+`;
 export default Home;
