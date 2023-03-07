f5bf305e4c40f16861de0f61215b9b52e5b5904b
Kim-yujin <w2117@e-mirim.hs.kr>
2023-03-07 22:23:57 +0900

    폴더 정리

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Home from './components/pages/Home';
 
 function App() {
     return (
-<Router>
-    <Switch>
-      <Route path='/' exact component={Home} />
-      <Route path="*" element={<h1>404 페이지</h1>}/>
-    </Switch>
-	</Router>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path="*" element={<h1>404 페이지</h1>}/>
+        </Switch>
+      </Router>
     );
 }
 
