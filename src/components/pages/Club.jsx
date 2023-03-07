b7593dd1ac4988516015549248a7e24db0ee0cde
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-03-08 00:32:41 +0900

    add: club page button

--- src/components/pages/Club.jsx
+++ src/components/pages/Club.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
 function Club(props) {
-    return (
-        <body style={{bgcolor: 'black',}}>
-            <h1>asdfasdf</h1>
-        </body>
-    );
+  const history = useHistory();
+
+  const handler2 = () => {
+    history.push("/");
+  };
+
+  return (
+    <Container>
+      <homeButton />
+      <Title>MIRIM SOCIETY</Title>
+      <HomeButton onClick={handler2}>home</HomeButton>
+    </Container>
+  );
 }
+const Container = styled.div`
+  top: 0;
+  width: 100vw;
+  overflow: hidden;
+  height: 100vh;
+  background-color: #f3f3f3;
+`;
+const HomeButton = styled.button`
+  cursor: pointer;
+  background-color: #f3f3f3;
+  border: none;
+`;
+const Title = styled.p`
+  font-size: 64px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  color: #475467;
+`;
+const Cotent = styled.p``;
 export default Club;
