399e45d807c15804b614a5314bcea7bfdb80dfce
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-03-07 22:52:32 +0900

    add: club page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
+import Club from './components/pages/club';
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path="*" element={<h1>404 페이지</h1>}/>
+          <Route path="/club" component={Club}/>
         </Switch>
       </Router>
     );
