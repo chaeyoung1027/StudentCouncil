35d4e121533dd84c7d501c37a0a6ae88ccd9bf37
Kim-yujin <w2117@e-mirim.hs.kr>
2023-03-08 02:15:57 +0900

    Home 1차 완성

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import image from '../../img/mainpage.png';
 import imagecircle from '../../img/circle.png';
 import './home.css';
 
 function Home(props) {
+    const [width, setWidth] = useState('43vw');
+    const [height, setHeight] = useState('43vw');
+    const [marginTop, setMarginTop] = useState('-53vw');
+    const [color, setColor] = useState('#F5F8FF');
     return (
         <Container>
             <Img src ={image}></Img>
-            <Img2 src ={imagecircle}></Img2>
-            <Text>MIRIM ALBUM</Text>
+            <Img2 src ={imagecircle} className='img2' style={{width, height,marginTop}} onClick={() => {
+                    setWidth('90vw');
+                    setHeight('90vw');
+                    setMarginTop('-80vw');
+                    setColor('#161616');
+                }}></Img2>
+            <Text style={{color}}>MIRIM ALBUM</Text>
         </Container>
     )
 }
@@ -27,21 +36,19 @@ const Img = styled.img`
     justify-content: center;
     margin-top: -2.5vw;`;
 const Img2 = styled.img`
-    width: 43vw;
-    height: 43vw;
     display: flex;
     justify-content: center;
     margin: 0 auto;
-    margin-top: -53vw;`;
+    transition: all ease 2s 0s;`;
 const Text = styled.div`
     font-size: 4.5vw;
     letter-spacing: 0.65rem;
     font-weight: bold;
-    font-family: 'Noto Sans KR', sans-serif;
-    color: #F5F8FF;  
+    font-family: 'Noto Sans KR', sans-serif; 
     display: flex;
     justify-content: center;
     margin: 0 auto;
-    margin-top: -25vw;  
+    margin-top: -25vw; 
+    transition: all ease 2s 0s; 
 `;
 export default Home;
