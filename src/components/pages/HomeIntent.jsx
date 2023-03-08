d8487f4376d52f34e23bd51f757f688ca6ff6ffb
Kim-yujin <w2117@e-mirim.hs.kr>
2023-03-08 17:57:12 +0900

    home carusel edit

--- src/components/pages/HomeIntent.jsx
+++ src/components/pages/HomeIntent.jsx
@@ -1,14 +1,21 @@
-import React, {useState} from 'react';
+import React, {useState,Component } from 'react';
+import { Carousel } from 'react-circular-carousel'
 import styled from 'styled-components';
-import image from '../../img/mainpage.png';
-import imagecircle from '../../img/circle.png';
+import image from '../../img/first.png';
 import { useHistory } from "react-router-dom";
-
-function Home(props) {
+function HomeIntent(props) {
     return (
         <Container>
             <Text>TEXTINPUT</Text>
+                <Carousel height={320} width={120} spacing={500} gab={20} offset={36} >
+                    <Img src={image}></Img>
+                    <Img src={image}></Img>
+                    <Img src={image}></Img>
+                    <Img src={image}></Img>
+                    <Img src={image}></Img>
+                </Carousel>
         </Container>
+        
     )
 }
 const Container = styled.div`
@@ -20,11 +27,15 @@ const Container = styled.div`
 `;
 const Text = styled.div`
     color: #FFFFFF;
-    font-size: 3vw;
+    font-size: 3.2vw;
     text-align: center;
     margin-top: 10vw;
     letter-spacing: 4rem;
     font-weight: bold;
     font-family: 'Noto Sans KR', sans-serif; 
 `;
-export default Home;
+const Img = styled.img`
+    widht: 100%;
+    height: 100%;
+    object-fit: cover;`;
+export default HomeIntent;
