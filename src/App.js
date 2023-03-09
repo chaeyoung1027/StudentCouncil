5f3c8d09f7a6a6baf188f43085725b4ff4bd66fc
yday05506 <yangdayeon2@gmail.com>
2023-03-09 18:46:01 +0900

    학교 행사 경로

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import HomeIntent from './components/pages/HomeIntent';
 import Club from './components/pages/Club';
+import SchEvent from './components/pages/SchoolEvent';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/homeintent' component={HomeIntent} />
           <Route path='/club' component={Club} />
+          <Route path='/schoolevent' component={SchEvent} />
           <Route path="*" element={<h1>404 페이지</h1>}/>
         </Switch>
       </Router>
