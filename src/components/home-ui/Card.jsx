4b6984263d48fc4e2fd26c997f0b5115d6c30885
olsi10 <wangbampang2035@gmail.com>
2023-03-13 06:08:42 +0900

    컴포넌트 X -> hmtl 코드로 불러오기

--- src/components/home-ui/Card.jsx
+++ src/components/home-ui/Card.jsx
@@ -1,24 +1,66 @@
-import React from "react";
-import styled from "styled-components";
-import image from './first.png';
+// import React from "react";
+// import styled from "styled-components";
 
-function Card(props) {
-    const { img, contents } = props;
+// function Card(props) {
+//     const { img, left, right} = props;
 
-    return <StyleCard>
-            <Img src={image}></Img>
-           </StyleCard>
-}
+//     return  <article>
+//                 <div className="inner">
+//                 <div className="pic">
+//                     <div className="dot"></div>
+//                 </div>
 
-const StyleCard = styled.div`
-    width: 200px;
-    height: 280px;
-    background-color: #fff;
-    border-radius: 15px;
-    object-fit: cover;
-`;
-const Img = styled.div`
-    width: 100%;
-`;
+//                 <div className="txt">
+//                     <h2>Calm</h2>
+//                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+//                     <ul>
+//                         <li className="pause">
+//                             <i className="fas fa-pause"></i>
+//                         </li>
+//                         <li clasNames="play">
+//                             <i className="fas fa-play"></i>
+//                         </li>
+//                         <li className="load">
+//                             <i className="fas fa-redo-alt"></i>
+//                         </li>
+//                     </ul>
+//                     <audio src="music/Blizzards.mp3"></audio>
+//                 </div>
+//                 </div>
+//             </article>
+// }
 
-export default Card;
+// const StyleCard = styled.div`
+//     width: 370px;
+//     height: 400px;
+//     background-color: #fff;
+//     border-radius: 15px;
+//     object-fit: cover;
+//     position: relative;
+// `;
+// const Img = styled.div`
+//     width: 100%;
+// `;
+// const Text = styled.div`
+//     width: 624px;
+//     height: 350px;
+//     background-color: rgba(0, 0, 0, 0.25);
+//     /* border: 1px solid red; */
+//     display: flex;
+//     -webkit-box-pack: justify;
+//     justify-content: space-between;
+//     position: absolute;
+//     color: red;
+//     font-size: 42px;
+//     top: -5px;
+//     left: -143px;
+//     z-index: 99;
+// `;
+// const Left = styled.div`
+//     text-align: left;
+// `;
+// const Right = styled.div`
+//     text-align: right;
+// `;
+
+// export default ;
--- src/components/pages/HomeIntent.jsx
+++ src/components/pages/HomeIntent.jsx
@@ -1,86 +1,279 @@
 import React, {useState,Component } from 'react';
-import { Carousel } from 'react-circular-carousel'
+// import { Carousel } from 'react-circular-carousel'
 import styled from 'styled-components';
 import { useHistory } from "react-router-dom";
-import Card from "../home-ui/Card";
+// import Card from "../home-ui/Card";
 
 function HomeIntent(props) {
-
-    function Handler() {
-        const frame = document.querySelector("section");
-        const lists = frame.querySelectorAll("article");
-        const audio = frame.querySelectorAll("audio");
-        const prev = document.querySelector(".btnPrev");
-        const next = document.querySelector(".btnNext");
-        const deg = 45; //각각의 article요소가 회전할 각도
-        const len = lists.length - 1; //순번이 0부터 시작하므로 전체 개수에서 1을 빼줌
-        let i = 0;
-        let num = 0;
-        let active = 0;
-
-        function activation(index, lists) {
-            for (let el of lists) {
-                el.classList.remove("on");
-            }
-                lists[index].classList.add("on");
-        }
-
-        //article의 개수만큼 반복
-        for (let el of lists) {
-            let pic = el.querySelector(".pic");
-            //각 article 요소를 45도씩 회전하고 아래로 배치
-            el.style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
-            pic.style.backgroundImage = `url(../../img/first.png)`;
-            i++;
-        }
-
-        //모든 오디오 요소를 반복하면서 정지시키고 .pic 요소의 모션을 중지해서 초기화하는 함수
-        function initMusic() {
-        for (let el of audio) {
-            el.closest("article").querySelector(".pic").classList.remove("on");
-            }
-        }
-
-        //prev 버튼 클릭 시
-        prev.addEventListener("click", () => {
-            //num값을 증가시키며 frame 45도 만큼 증가시키며 시계 방향으로 계속 회전
-            num++;
-            frame.style.transform = `rotate(${deg * num}deg)`;
-            active == 0 ? (active = len) : active--;
-            activation(active, lists);
-        });
-
-        //next 버튼 클릭시
-        next.addEventListener("click", () => {
-            //num값을 감소시키며 frame을 45도 만큼 감소시키며 반시계 방향으로 회전
-            num--;
-            frame.style.transform = `rotate(${deg * num}deg)`;
-            active == len ? (active = 0) : active++;
-            activation(active, lists);
-        });
-
-    }
+    
+    Handler();
 
     return (
         <Container>
+            Handler();
             <Text>TEXTINPUT</Text>
-                <Section1>
-                    <Section2 className='section'>
-                        <Card className='article'></Card>
-                        <Card className='article'></Card>
-                        <Card className='article'></Card>
-                        <Card className='article'></Card>
-                        <Card className='article'></Card>
-                        <Card className='article'></Card>
-                    </Section2>
-                    <Prev><span className='prev'>이전</span></Prev>
-                    <Next><span className='next'>다음</span></Next>
-                </Section1>
+            <figure>
+                <section>
+                <article className="on">
+                    <div className="inner">
+                    <div className="pic">
+                        <div className="dot"></div>
+                    </div>
+
+                    <div className="txt">
+                        <h2>Blizzards</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <ul>
+                        <li className="pause">
+                            <i className="fas fa-pause"></i>
+                        </li>
+                        <li className="play">
+                            <i className="fas fa-play"></i>
+                        </li>
+                        <li className="load">
+                            <i className="fas fa-redo-alt"></i>
+                        </li>
+                        </ul>
+                        <audio src="music/Blizzards.mp3"></audio>
+                    </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div className="inner">
+                    <div className="pic">
+                        <div className="dot"></div>
+                    </div>
+
+                    <div className="txt">
+                        <h2>Calm</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <ul>
+                        <li className="pause">
+                            <i className="fas fa-pause"></i>
+                        </li>
+                        <li className="play">
+                            <i className="fas fa-play"></i>
+                        </li>
+                        <li className="load">
+                            <i className="fas fa-redo-alt"></i>
+                        </li>
+                        </ul>
+                    </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div className="inner">
+                    <div className="pic">
+                        <div className="dot"></div>
+                    </div>
+
+                    <div className="txt">
+                        <h2>Dusty Road</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <ul>
+                        <li className="pause">
+                            <i className="fas fa-pause"></i>
+                        </li>
+                        <li className="play">
+                            <i className="fas fa-play"></i>
+                        </li>
+                        <li className="load">
+                            <i className="fas fa-redo-alt"></i>
+                        </li>
+                        </ul>
+                        <audio src="music/Dusty_Road.mp3"></audio>
+                    </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div className="inner">
+                    <div className="pic">
+                        <div className="dot"></div>
+                    </div>
+
+                    <div className="txt">
+                        <h2>Escape</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <ul>
+                        <li className="pause">
+                            <i className="fas fa-pause"></i>
+                        </li>
+                        <li className="play">
+                            <i className="fas fa-play"></i>
+                        </li>
+                        <li className="load">
+                            <i className="fas fa-redo-alt"></i>
+                        </li>
+                        </ul>
+                        <audio src="music/Escape.mp3"></audio>
+                    </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div className="inner">
+                    <div className="pic">
+                        <div className="dot"></div>
+                    </div>
+
+                    <div className="txt">
+                        <h2>Payday</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <ul>
+                        <li className="pause">
+                            <i className="fas fa-pause"></i>
+                        </li>
+                        <li className="play">
+                            <i className="fas fa-play"></i>
+                        </li>
+                        <li className="load">
+                            <i className="fas fa-redo-alt"></i>
+                        </li>
+                        </ul>
+                        <audio src="music/Payday.mp3"></audio>
+                    </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div className="inner">
+                    <div className="pic">
+                        <div className="dot"></div>
+                    </div>
+
+                    <div className="txt">
+                        <h2>Retreat</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <ul>
+                        <li className="pause">
+                            <i className="fas fa-pause"></i>
+                        </li>
+                        <li className="play">
+                            <i className="fas fa-play"></i>
+                        </li>
+                        <li className="load">
+                            <i className="fas fa-redo-alt"></i>
+                        </li>
+                        </ul>
+                        <audio src="music/Retreat.mp3"></audio>
+                    </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div className="inner">
+                    <div className="pic">
+                        <div className="dot"></div>
+                    </div>
+
+                    <div className="txt">
+                        <h2>Seasonal</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <ul>
+                        <li className="pause">
+                            <i className="fas fa-pause"></i>
+                        </li>
+                        <li className="play">
+                            <i className="fas fa-play"></i>
+                        </li>
+                        <li className="load">
+                            <i className="fas fa-redo-alt"></i>
+                        </li>
+                        </ul>
+                        <audio src="music/Seasonal.mp3"></audio>
+                    </div>
+                    </div>
+                </article>
+
+                <article>
+                    <div className="inner">
+                    <div className="pic">
+                        <div className="dot"></div>
+                    </div>
+
+                    <div className="txt">
+                        <h2>Vespers</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                        <ul>
+                        <li className="pause">
+                            <i className="fas fa-pause"></i>
+                        </li>
+                        <li className="play">
+                            <i className="fas fa-play"></i>
+                        </li>
+                        <li className="load">
+                            <i className="fas fa-redo-alt"></i>
+                        </li>
+                        </ul>
+                        <audio src="music/Vespers.mp3"></audio>
+                    </div>
+                    </div>
+                </article>
+                </section>
+            </figure>
+                <Prev className='btnPrev'><span className='prev'>이전</span></Prev>
+                <Next className='btnNext'><span className='next'>다음</span></Next>
             <Circle></Circle>
         </Container>
         
     )
+
 }
+
+function Handler() {
+    const frame = document.querySelector("section");
+    const lists = frame.querySelectorAll("article");
+    const audio = frame.querySelectorAll("audio");
+    const prev = document.querySelector(".btnPrev");
+    const next = document.querySelector(".btnNext");
+    const deg = 45; //각각의 article요소가 회전할 각도
+    const len = lists.length - 1; //순번이 0부터 시작하므로 전체 개수에서 1을 빼줌
+    let i = 0;
+    let num = 0;
+    let active = 0;
+    
+    function activation(index, lists) {
+      for (let el of lists) {
+        el.classList.remove("on");
+      }
+      lists[index].classList.add("on");
+    }
+    
+    //article의 개수만큼 반복
+    for (let el of lists) {
+      let pic = el.querySelector(".pic");
+      //각 article 요소를 45도씩 회전하고 아래로 배치
+      el.style.transform = `rotate(${deg * i}deg) translateY(-100vh)`;
+      i++;
+    }
+    
+    //prev 버튼 클릭 시
+    prev.addEventListener("click", () => {
+    
+      //num값을 증가시키며 frame 45도 만큼 증가시키며 시계 방향으로 계속 회전
+      num++;
+      frame.style.transform = `rotate(${deg * num}deg)`;
+    
+      active == 0 ? (active = len) : active--;
+      activation(active, lists);
+    });
+    
+    //next 버튼 클릭시
+    next.addEventListener("click", () => {
+    
+      //num값을 감소시키며 frame을 45도 만큼 감소시키며 반시계 방향으로 회전
+      num--;
+      frame.style.transform = `rotate(${deg * num}deg)`;
+    
+      active == len ? (active = 0) : active++;
+      activation(active, lists);
+    });
+    
+    
+}    
 
 const Container = styled.div`
     top: 0;
@@ -94,7 +287,7 @@ const Text = styled.div`
     color: #FFFFFF;
     font-size: 3.2vw;
     text-align: center;
-    margin-top: 10vw;
+    margin-top: 7vw;
     letter-spacing: 4rem;
     font-weight: bold;
     font-family: 'Noto Sans KR', sans-serif; 
@@ -102,49 +295,46 @@ const Text = styled.div`
 
 // [12 file figure tag]
 // 전체를 감싸는 용도
-const Section1 = styled.div`
-	width: 100%;
-	height: 100vh;
-	min-height: 600px;
-	overflow: hidden;
-	position: relative;    
-`;
-// [12 file section tag]
-// 이미지를 감싸는 용도
-const Section2 = styled.div`
-    display: flex;
-    width: 50vw;
-	height: 65vh;
-	position: absolute;
-	left: 35%;
-	top: 38%;
-	margin-top: -25vh;
-	margin-left: -10vw;	
-	transition: 1s;
-    gap : 20px;
-`;
-
-const Img = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: cover;`;
+// const Section1 = styled.div`
+// 	width: 100%;
+// 	height: 100vh;
+// 	min-height: 600px;
+// 	overflow: hidden;
+// 	position: relative;
+//     overflow: hidden;
+// `;
+// // [12 file section tag]
+// // 이미지를 감싸는 용도
+// const Section2 = styled.div`
+//     display: flex;
+//     width: 100%;
+// 	height: 65vh;
+// 	position: absolute;
+// 	left: 9.5%;
+// 	top: 38%;
+// 	margin-top: -25vh;
+// 	margin-left: -10vw;	
+// 	transition: 1s;
+//     gap : 50px;
+// `;
 const Circle = styled.div`
   width: 80vw;
   height: 80vw;
   background-color: #ffffff;
   border-radius: 100rem;
   position: absolute;
-  top: 50rem;
+  top: 44rem;
   left: 12rem;
 `;
 
 // 이전 버튼
 const Prev = styled.div`
+    z-index: 99;
 	width: 60px;
 	height: 60px;
 	position: absolute;
-	top: 50%;
-	left: 50%;
+	top: 70%;
+	left: 55%;
 	transform: translate(-20vw, -50%);
 	display: flex;
 	align-items: center;
@@ -192,11 +382,12 @@ const Prev = styled.div`
 
 // 다음버튼
 const Next = styled.div`
+    z-index: 99;
 	width: 60px;
 	height: 60px;
 	position: absolute;
-	top: 50%;
-	right: 50%;
+	top: 70%;
+	right: 53.5%;
 	transform: translate(20vw, -50%);
 	display: flex;
 	align-items: center;
